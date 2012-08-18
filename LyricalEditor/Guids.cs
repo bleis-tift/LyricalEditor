ae2709fd9890f3e6bbcc068981dfdb04e74ebaf8
bleis-tift <bleis-tift@hotmail.co.jp>
2012-08-19 01:35:57 +0900

    first commit

---
+++ LyricalEditor/Guids.cs
@@ -0,0 +1,14 @@
+﻿// Guids.cs
+// MUST match guids.h
+using System;
+
+namespace bleistift.LyricalEditor
+{
+    static class GuidList
+    {
+        public const string guidLyricalEditorPkgString = "5e80449c-5c27-4128-8848-c4ae6e3c27ee";
+        public const string guidLyricalEditorCmdSetString = "75f5f8bc-dbf9-4e73-8df0-65cf4681eadc";
+
+        public static readonly Guid guidLyricalEditorCmdSet = new Guid(guidLyricalEditorCmdSetString);
+    };
+}
